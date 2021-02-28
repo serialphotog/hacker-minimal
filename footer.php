feac46e77164b123546a940089b5dade27ffbccc
Adam Thompson <adam@serialphotog.com>
2021-02-27 19:56:33 -0500

    Simple footer implementation

--- footer.php
+++ footer.php
@@ -1,4 +1,13 @@
-    </container>
+    </div>
+
+    <footer class="container" id="footer">
+        <div class="row" id="footer-inner">
+            <p class="col" id="copyright">
+                Copyright &copy; <?php echo date('Y'); ?> <a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a>. All rights reserved.
+            </p>
+        </div>
+    </footer>
+
     <?php wp_footer(); ?>
 </body>
 </html>
