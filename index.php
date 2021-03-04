aeb60ab4e7e90fcc327b650a41bb11117ee0baa1
Adam Thompson <adam@serialphotog.com>
2021-03-04 14:26:50 -0500

    Simple excerpt content loop

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php get_header(); ?>
 
+    <div class="row">
+        <div class="column column-80" id="content-area">
+            <?php 
+                if (have_posts()) {
+                    while (have_posts()) {
+                        the_post();
+                        
+                        get_template_part('parts/content-excerpt'); 
+                    }
+                }
+            ?>
+        </div>
+        <div class="column" id="sidebar-area">
+            <?php get_sidebar(); ?>
+        </div>
+    </div>
+
 <?php get_footer(); ?>
